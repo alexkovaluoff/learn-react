b54ef36444f7ad958936098dfa6165091f2d8b12
Aliaksandr Kavaliou <alexkovaluoff@gmail.com>
2022-02-09 11:42:48 +0300

    routing changed to NavLink, added styles to hyperlinks, added paddings

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.css'
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <div className={classes.item}>
-                <a href='/profile'>Profile</a>
+                <NavLink className={(navData) => navData.isActive ? classes.active : "" } to='/profile'>Profile</NavLink>
             </div>
             <div className={classes.item}>
-                <a href='/messages'>Messages</a>
+                <NavLink className={(navData) => navData.isActive ? classes.active : "" } to='/messages'>Messages</NavLink>
             </div>
             <div className={classes.item}>
-                <a href='/news'>News</a>
+                <NavLink className={(navData) => navData.isActive ? classes.active : "" } to='/news'>News</NavLink>
             </div>
             <div className={classes.item}>
-                <a href='/music'>Music</a>
+                <NavLink className={(navData) => navData.isActive ? classes.active : "" } to='/music'>Music</NavLink>
             </div>
             <div className={classes.item}>
-                <a href='/settings'>Settings</a>
+                <NavLink className={(navData) => navData.isActive ? classes.active : "" } to='/settings'>Settings</NavLink>
             </div>
         </nav>
     );
