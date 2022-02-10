890a4d49776c6f1cd25bfdb2f32f4d46f76c0ec5
Aliaksandr Kavaliou <alexkovaluoff@gmail.com>
2022-02-10 16:45:25 +0300

    add soundcloud track

--- src/components/Music/Music.tsx
+++ src/components/Music/Music.tsx
@@ -4,10 +4,12 @@ import classes from "./Music.module.css";
 const Music = (props: any) => {
     return (
         <div>
-            <iframe className={classes.playerStyle} frameBorder="0" src="https://music.yandex.ru/iframe/#track/93390818/18809983">Слушайте <a
-                href='https://music.yandex.ru/album/18809983/track/93390818'>МАЛИНОВАЯ ЛАДА</a> — <a
-                href='https://music.yandex.ru/artist/6229247'>GAYAZOV$ BROTHER$</a> на Яндекс.Музыке
-            </iframe>
+            <div>
+                <iframe className={classes.playerYandexStyle} frameBorder="0" src="https://music.yandex.ru/iframe/#track/93390818/18809983"/>
+            </div>
+            <div>
+                <iframe className={classes.playerSCStyle} frameBorder='0' src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1196364361&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+            </div>
         </div>
     )
 }
